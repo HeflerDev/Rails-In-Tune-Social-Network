2a65b20ef4404d9aa83404b7f225afcd97137345
HeflerDev <hheflerdevelopment@gmail.com>
2020-05-20 18:39:31 -0300

    More tests to follow_spec

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Follow, type: :model do
     it { should belong_to(:user_following) }
     it { should belong_to(:user_followed) }
   end
+
+  describe 'methods' do
+    it "properly works" do
+      user_one = User.create(username:'Example', fullname:'FullExample')
+      user_two = User.create(username:'Example2', fullname:'FullExample2')
+      Follow.create(follower_id: user_one.id, followee_id: user_two.id)
+      expect(user_one.is_following?(user_two)).to be true
+      expect(user_two.is_following?(user_one)).to be false
+      expect(user_two.is_being_followed?(user_one)).to be true
+      expect(user_one.is_being_followed?(user_two)).to be false
+
+    end
+  end
 end
