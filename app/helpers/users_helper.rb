2616f9d39ecb0b05f5d2a6d81dcd0d3b8291598d
HeflerDev <hheflerdevelopment@gmail.com>
2020-05-22 19:47:31 -0300

    User Helper: Correct 'suggest_friend' method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,16 @@
 module UsersHelper
   def get_random_user
-    user = nil
+    user = User.find(rand(1..User.count))
     while !user
       user = User.find(rand(1..User.count))
     end
     user
+  end
+
+  def suggest_friend
+    loop do
+      user = get_random_user
+      return user if !current_user.followees.include?(user) && current_user != user
+    end
   end
 end
